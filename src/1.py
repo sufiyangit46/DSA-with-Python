d4e4028c6ad4928481259628eb553ce08f91b9e3
Mohd Sufiyan <shaikhsufiyan1246@gmail.com>
2026-02-09 23:04:19 +0530

    D3

--- src/1.py
+++ src/1.py
@@ -8,3 +8,27 @@ print(last)
 remove=n//10
 print(remove)
 
+
+#count the number of digit
+
+n=8754685
+count=0
+while n>0:
+    n=n//10
+    count+=1
+print(count)
+
+# to count number of digit we can use log base 10 and add 1 and then convert to integer
+
+from math import *
+n=5468
+def digit(n):
+    return int(log10(n))+1
+print(digit(n))
+
+
+
+
+
+
+
