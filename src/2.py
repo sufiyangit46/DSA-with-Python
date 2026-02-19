2416e368bf75aee5ec3dde7292d694ce9f9cb182
Mohd Sufiyan <shaikhsufiyan1246@gmail.com>
2026-02-19 07:55:36 +0530

    Fibonacci

---
+++ src/2.py
@@ -0,0 +1,30 @@
+#11
+# Find the Fibonacci number using Recursion
+
+a=int(input())
+def fibo(a):
+    if a==0 or a==1:
+        return a
+    return fibo(a-1)+fibo(a-2)
+print(fibo(a))
+
+# Using While loop
+
+s=int(input())
+count=0
+a,b=0,1
+while count<s:
+    print(a,end=" ")
+    a,b=b,a+b
+    count+=1
+print(a)
+
+# Using For loop
+
+s=int(input())
+a,b=0,1
+for i in range(s):
+    print(a,end=" ")
+    a,b=b,a+b
+print(a)
+
