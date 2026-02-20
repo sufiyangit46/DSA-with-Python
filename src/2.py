e60a8c7a4fda45c08f551fbc52f68b07c239aa48
Mohd Sufiyan <shaikhsufiyan1246@gmail.com>
2026-02-20 08:25:24 +0530

    Selection Sort

--- src/2.py
+++ src/2.py
@@ -29,3 +29,16 @@ for i in range(s):
 print(a)
 
 # Selection Sort
+
+nums=[5,1,8,2,4,7,9,3]
+def selection(nums):
+    n=len(nums)
+    for i in range(n):
+        min_ind=i
+        for j in range(i+1,n):
+            if nums[j]<nums[min_ind]:
+                min_ind=j
+        nums[i],nums[min_ind]=nums[min_ind],nums[i]
+    return nums
+print(selection(nums))
+
