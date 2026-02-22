a78aaf9b134cba59371c508bf9cd455eb129c3a7
Mohd Sufiyan <shaikhsufiyan1246@gmail.com>
2026-02-22 16:41:06 +0530

    Merge Sort

--- src/2.py
+++ src/2.py
@@ -67,6 +67,49 @@ def insertion(nums):
             j-=1
         nums[j+1]=key
     return nums
+print(insertion(nums))
+
+#12
+# Merge two sorted array
+# Merge Array
+
+left=[1,2,3,4]
+right=[1,1,3,4,5,6,7]
+def merge_array(left,right):
+    result=[]
+    i,j=0,0
+    n,m=len(left),len(right)
+    while i<n and j<m:
+        if left[i]<=right[j]:
+            result.append(left[i])
+            i+=1
+        else:
+            result.append(right[j])
+            j+=1
+    if i<n:
+        while i<n:
+            result.append(left[i])
+            i+=1
+    if j<m:
+        while j<m:
+            result.append(right[j])
+            j+=1
+    return result
+print(merge_array(left,right))
+
+# Merge Sort
+
+nums=[3,1,2,4,1,5,2,6,4]
+def merge_sort(nums):
+    if len(nums)<=1:
+        return nums
+    mid=len(nums)//2
+    left_arr=nums[:mid]
+    right_arr=nums[mid:]
+    left=merge_sort(left_arr)
+    right=merge_sort(right_arr)
+    return merge_array(left,right)
+print(merge_sort(nums))
 
 
 
