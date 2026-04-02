0b1895ac26456c8d25b3d1176e73bcf418eafcde
Mohd Sufiyan <shaikhsufiyan1246@gmail.com>
2026-04-02 17:30:11 +0530

    two sum optimal

--- src/3.py
+++ src/3.py
@@ -22,3 +22,16 @@ for i in range(0,n-1):
     for j in range(i+1,n):
         if nums[i]+nums[j]==target:
             print([i,j])              # TC O(N2) AND SC O(1)
+
+# Optimal Method
+
+nums=[2,5,2,6,9,4,5,2]
+n=len(nums)
+hashmap={}
+target=13
+for i in range(0,n):
+    remaining=target-nums[i]
+    if remaining in hashmap:
+        print([hashmap[remaining],i])
+    else:
+        hashmap[nums[i]]=i              # TC O(N) AND SC O(N)
