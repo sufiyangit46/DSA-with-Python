3691f7325fefe4dd84fff39034e5e721a00482ac
Mohd Sufiyan <shaikhsufiyan1246@gmail.com>
2026-07-18 18:48:06 +0530

    2 question

--- src/50 questions.py
+++ src/50 questions.py
@@ -15,3 +15,21 @@ print(''.join(char))
 # TWO POINTER TECHNIQUE IS THE APPROACH TO SOLVE THIS QUESTION
 
 
+# 2 CHECK WHETHER A STRING IS A PALINDROME
+
+num=(input())
+a=num
+n=len(a)
+i=0
+j=n-1
+while i<j:
+    if a[i]!=a[j]:
+        print("Not Palindrome")
+        break
+    i+=1
+    j-=1
+else:
+    print("Palindrome")
+
+# TC O(N) AND SC O(1)
+# TWO POINTER TECHNIQUE IS THE APPROACH TO CHECK WHETHER IT IS PALINDROME OR NOT
